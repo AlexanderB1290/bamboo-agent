25a257a444974e48d919c5c4e2a0ac6ccb1d541b
Alexander Bushev <alexander.bushev1290@gmail.com>
2022-10-23 13:32:41 +0300

    add install scripts

---
+++ scripts/install/docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add GPG key
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Set repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Install engine
+apt-get update
+apt-get install -y \
+  docker-ce docker-ce-cli \
+  containerd.io docker-compose-plugin
