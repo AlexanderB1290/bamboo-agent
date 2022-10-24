db66c275dc2fe38e6b95c7da0d7c336db5ce9ce1
Alexander Bushev <alexander.bushev1290@gmail.com>
2022-10-24 10:47:40 +0300

    Update install.all.sh

--- scripts/install.all.sh
+++ scripts/install.all.sh
@@ -3,6 +3,11 @@
 {
   /scripts/install/prerequsites.sh
   /scripts/install/docker.sh
+	
+	# Clean up installations
+	apt-get clean autoclean \
+		&& apt-get autoremove -y \
+		&& rm -rf /var/lib/apt/lists/*
 } || {
   echo "Something went wrong"
   exit 1
